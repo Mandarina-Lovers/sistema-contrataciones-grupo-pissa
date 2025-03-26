9d236348cb306875290595968e13fbc9e21aa9da
Daniel Arteaga <71586064+D4nyArt@users.noreply.github.com>
2025-03-26 15:46:05 -0600

    disabled analytics

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import {getDatabase} from "firebase/database";
 import { getStorage } from "firebase/storage";
 
@@ -23,7 +23,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
 const database = getDatabase(app);
 const storage = getStorage(app)
 
