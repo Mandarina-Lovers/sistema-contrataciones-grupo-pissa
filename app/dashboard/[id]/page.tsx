b4d69509cf63f9725996adbb5317f9c382b39327
Gabriel <59619906+Gabriel-Ponce@users.noreply.github.com>
2025-04-03 10:05:37 -0600

    User Information screen added

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,25 +1,9 @@
-"use client";
-import React from "react";
-import { useRouter } from "next/compat/router";
+import ListInformation from '@/app/components/userinfo'
 
-export default function User() {
-  const router = useRouter();
-  const handleLink = () => {
-  }  
-  
-  return (
-    <div className="flex flex-col items-center  p-6 min-h-screen w-300">
-      <h1 className="text-4xl font-extrabold text-black mb-6">User Information</h1>
-      <div className="bg-white shadow-xl rounded-2xl p-6 w-96 text-center transform transition duration-500 hover:scale-105">
-        <img
-          src="https://via.placeholder.com/150"
-          alt="Profile"
-          className="w-32 h-32 rounded-full mx-auto mb-4 border-4 border-blue-500 shadow-md"
-        />
-        <h2 className="text-2xl font-bold text-gray-800">{"lorem ipsum"}</h2>
-        <p className="text-gray-600 text-sm">{"lorem ipsum"}</p>
-        <p className="mt-4 text-gray-700 text-md italic">{"lorem ipsum"}</p>
-      </div>
-    </div>
-  );
+export default function UserInformation() {
+
+return(<>
+<ListInformation/>
+</>);
+
 }
