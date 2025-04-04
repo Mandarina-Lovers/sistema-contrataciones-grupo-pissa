61d7652cb24ec8d17bd221f2229f185c94adaa54
Andrea Bahena <andreabahenavs@gmail.com>
2025-04-03 18:02:49 -0600

    Ícono mostrar contraseñas

--- app/components/form-login.tsx
+++ app/components/form-login.tsx
@@ -6,11 +6,15 @@ import { useRouter } from "next/navigation";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebaseConfig";
 
+// Íconos 
+import { Eye, EyeOff } from "lucide-react";
+
 
 export default function Formulario() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [mostrarContraseña, setShowPassword] = useState(false);
 
   const router = useRouter();
 
@@ -68,15 +72,28 @@ export default function Formulario() {
           required
         />
       </div>
-      <div className="mb-4">
+      <div className="mb-4 relative"> 
         <input
-          type="password"
+          type={mostrarContraseña ? "text" : "password"}
           className="w-full p-2 border border-gray-300 rounded-lg mt-1 text-black bg-[#fafbfc]"
           placeholder="Contraseña"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          />
+        />
+        {password.length > 0 && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!mostrarContraseña)}
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+          >
+            {mostrarContraseña ? (
+              <EyeOff size={20} />
+            ) : (
+              <Eye size={20} />
+            )}
+          </button>
+        )}
       </div>
       <div>
         <button
