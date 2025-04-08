e1246399c9c29b96e2182daa015a11d05553e0e7
Daniel Arteaga <dandavarteaga@gmail.com>
2025-04-08 03:56:04 -0600

    Codigo inicial del expediente de cada candidato (vista de admin)

--- app/dashboard/[id]/page.tsx
+++ app/dashboard/[id]/page.tsx
@@ -1,12 +1,17 @@
+"use client"
+import { usePathname } from "next/navigation";
 import ListInformation from '@/app/components/userinfo'
 import Usuarios from '@/app/components/usuario';
+import ExpedienteCandidato from "@/app/components/expedienteCandidato";
 
 export default function UserInformation() {
-
+    const pathname = usePathname();
+    const id = pathname.split("/").pop(); // Extract the last segment of the path
 return(
     <>
         {/*<ListInformation/>*/}
         <Usuarios/>
+        <ExpedienteCandidato userId={id}/>
     </>);
 
 }
