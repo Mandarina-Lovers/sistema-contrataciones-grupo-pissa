a10e9a45fc5d71d3a64cb3762d47c4bbface1299
MrEstebato <tebananas@hotmail.com>
2025-04-11 22:59:32 -0600

    Dynamic portfolio

--- app/candidato/expediente/page.tsx
+++ app/candidato/expediente/page.tsx
@@ -1,11 +1,15 @@
 "use client";
+import { cookies } from "next/headers";
 import ExpedienteCandidato from "@/app/components/expedienteCandidato";
 
-export default function UserInformation() {
+export default async function UserInformation() {
+  const candidateCookies = await cookies();
+  const userID = candidateCookies.get("candidateId")?.value;
   return (
     <>
       <ExpedienteCandidato
-        userId={"JAT469lLXCZi8wi4dcq9xzNoVSu1"}
+        // userId={"JAT469lLXCZi8wi4dcq9xzNoVSu1"}
+        userId={userID}
         role="candidate"
       />
     </>
